b659b4d283eaa2d94d4911d13023008f54a87683
o4eretniy <s.o4eretniy@gmail.com>
2022-08-25 20:46:02 +0300

    Add files via upload
    Feat: start project

---
+++ weather-macOS/weather_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_macOSApp.swift
+//  weather-macOS
+//
+//  Created by Oleksandr Ocheretnii on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
